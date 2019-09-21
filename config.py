07f26351c0484f84d263c11f9cbd9335d45698fd
aline-bebe <ingabirealine40@gmail.com>
2019-09-21 13:38:07 +0200

    testing

--- config.py
+++ config.py
@@ -5,14 +5,20 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # QLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:New@localhost/pitches'
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:New@localhost/pitches_test'
+
+
 class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    QLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:News@localhost/pitches'
+    QLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:New@localhost/pitches'
 
 class DevConfig(Config):
     DEBUG = True
 
 config_options = {
     'development':DevConfig,
-    'production':ProdConfig
+    'production':ProdConfig,
+    'test':TestConfig
 }
