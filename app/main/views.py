5160351774d73391385b8171a7bfdf4a87930dc6
aline-bebe <ingabirealine40@gmail.com>
2019-09-21 19:56:49 +0200

    uploading image

--- app/main/views.py
+++ app/main/views.py
@@ -89,14 +89,24 @@ def view_pitch(id):
     return render_template('pitch.html', pitches=pitches, comment=comment, category_id=id)
 
 
-@main.route('/user/<uname>/update/pic', methods = ['POST'])
-@login_required
+
+
+@main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
     if user is None:
         abort(404)
-    
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
     form = UpdateProfile()
 
     if form.validate_on_submit():
@@ -105,8 +115,14 @@ def profile(uname):
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.profile', uname = user.username))
+        return redirect(url_for('.profile',uname=user.username))
 
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
@@ -114,4 +130,4 @@ def profile(uname):
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
 
-    return render_template("profile/profile.html", user = user)
+
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='allybebe'
 # export DATABASE_URL='postgresql+psycopg2://wecode:New@localhost/pitches'
-export MAIL_USERNAME=bebegigi222@gmail.com
-export MAIL_PASSWORD=bebeally?111
+export MAIL_USERNAME=ingabirealine40@gmail.com
+export MAIL_PASSWORD=0781140395
 
 python3.6 manage.py server
